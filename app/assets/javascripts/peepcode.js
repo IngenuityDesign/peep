c5ccb0874c898451b23223db059a96b086189965
Stephen Parente <stephen@ingenuitydesign.com>
2014-08-11 10:55:01 -0400

    JS for subscribe ajax

--- app/assets/javascripts/peepcode.js
+++ app/assets/javascripts/peepcode.js
@@ -16,3 +16,55 @@ ready = function() {
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
+
+jQuery(function ($) {
+
+  function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  } 
+
+  var form = $('form[name="mc-embedded-subscribe-form"]');
+  form.attr('action', 'javascript:void(0)');
+
+  form.on('submit', function (e) {
+    var email = $('input[name="EMAIL"]', this).val() || "";
+    if (validateEmail(email)) {
+      var response = $.getJSON( '/subscribe',
+      { email: email }, function( response, textStatus ) {
+        
+        if (response.status == "ok") {
+          // Successfully subscribed you to our mailing list
+        } else {
+          // Something went wrong. Status codes will tell us
+          switch (response.data.code) {
+            // Individual handlers for codes go here
+            case 401:
+              // We are already subscribed
+              break;
+            case 403:
+              // Invalid Email Address
+              break;
+            case 404:
+              // List doesn't exist. Something internal went wrong.
+              break;
+            case 0:
+              // Unknown error
+              break;
+            default:
+              // Anythin gelse
+              break;
+          } // End switch case
+
+        } // End response checking
+
+      }); // End getJSON 
+
+    } else {
+      // Email is invalid
+      alert("Email is invalid");
+    }
+    
+    e.stopPropagation();
+  })
+});
